e66962347f6785ae1814718d857795e929733e09
wnsrl1228 <wnsrl981228@gmail.com>
2022-07-15 15:34:49 +0900

    repository까지 구현

--- src/main/java/com/ll/exam/WiseSayingRepository.java
+++ src/main/java/com/ll/exam/WiseSayingRepository.java
@@ -1,11 +1,51 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingRepository {
-
+    int lastIndex;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
     Scanner sc;
     WiseSayingRepository(Scanner sc){
         this.sc = sc;
+        lastIndex = 0;
+    }
+
+    public WiseSaying write(String content, String author) {
+        int id = ++lastIndex;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+        return wiseSaying;
+    }
+
+    public boolean remove(int paramId) {
+        for (WiseSaying saying : wiseSayings) {
+            if (saying.id == paramId){
+                wiseSayings.remove(saying);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean modify(int paramId) {
+        for (WiseSaying saying : wiseSayings) {
+            if (saying.id == paramId){
+                System.out.println("명언(기존) : " + saying.content);
+                saying.content = sc.nextLine();
+                System.out.println("작가(기존) : " + saying.author);
+                saying.author = sc.nextLine();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void list() {
+        for (WiseSaying saying : wiseSayings) {
+            System.out.printf("%d / %s / %s\n",saying.id, saying.author, saying.content);
+        }
     }
 }
--- src/main/java/com/ll/exam/WiseSayingService.java
+++ src/main/java/com/ll/exam/WiseSayingService.java
@@ -5,47 +5,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class WiseSayingService {
-    int lastIndex;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
+
     Scanner sc;
+    WiseSayingRepository wiseSayingRepository;
     WiseSayingService(Scanner sc) {
         this.sc = sc;
-        lastIndex = 0;
+        wiseSayingRepository = new WiseSayingRepository(sc);
     }
 
     public WiseSaying write(String content, String author) {
-        int id = ++lastIndex;
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-        wiseSayings.add(wiseSaying);
-        return wiseSaying;
+        return wiseSayingRepository.write(content,author);
     }
 
     public boolean remove(int paramId) {
-        for (WiseSaying saying : wiseSayings) {
-            if (saying.id == paramId){
-                wiseSayings.remove(saying);
-                return true;
-            }
-        }
-        return false;
+        return wiseSayingRepository.remove(paramId);
+
     }
 
     public boolean modify(int paramId) {
-        for (WiseSaying saying : wiseSayings) {
-            if (saying.id == paramId){
-                System.out.println("명언(기존) : " + saying.content);
-                saying.content = sc.nextLine();
-                System.out.println("작가(기존) : " + saying.author);
-                saying.author = sc.nextLine();
-                return true;
-            }
-        }
-        return false;
+        return wiseSayingRepository.modify(paramId);
+
     }
 
     public void list() {
-        for (WiseSaying saying : wiseSayings) {
-            System.out.printf("%d / %s / %s\n",saying.id, saying.author, saying.content);
-        }
+        wiseSayingRepository.list();
     }
 }
